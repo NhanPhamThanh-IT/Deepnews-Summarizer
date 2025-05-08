15bde743cd98c932608ef2a684fdc9569ddf79e6
Nolan <vhnam04@gmail.com>
2025-05-08 10:30:41 +0700

    Add files via upload

---
+++ Frontend/data/news_data.py
@@ -0,0 +1,200 @@
+"""
+Sample news data
+"""
+
+NEWS_ARTICLES = [
+    {
+        "id": 1,
+        "title": "Hai Hồng y có cơ hội trở thành Giáo hoàng gốc Á đầu tiên",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1EifeT.img?w=612&h=304&q=60&m=6&f=jpg&x=204&y=304&u=t",
+        "source": "VnExpress",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB131kvb?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "9 giờ",
+        "content": "Hai Hồng y Charles Maung Bo và Luis Antonio Tagle có thể trở thành Giáo hoàng gốc châu Á đầu tiên khi Đức Giáo hoàng Francis nghỉ hưu.",
+        "likes": 22,
+        "comments": 5
+    },
+    {
+        "id": 2,
+        "title": "Vì sao các tăng ni đều cao đầu nhưng Đức Phật lại để tóc?",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1EgnBt.img?w=612&h=304&q=60&m=6&f=jpg&u=t",
+        "source": "VTC News",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBZJRUS?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "6 giờ",
+        "content": "Mặc dù là người sáng lập ra Phật giáo, nhưng hình tượng Đức Phật thường được miêu tả với mái tóc xoăn, trong khi các tăng ni đều cạo trọc đầu.",
+        "likes": 12,
+        "comments": 3
+    },
+    {
+        "id": 3,
+        "title": "Tổng Bí thư Tô Lâm dự lễ duyệt binh kỷ niệm Ngày Chiến thắng ở Nga",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1EiPMF.img?w=268&h=140&q=60&m=6&f=jpg&u=t",
+        "source": "Tuổi trẻ",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB9mld4?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "47 phút",
+        "content": "Tổng Bí thư Tô Lâm đã có mặt tại Quảng trường Đỏ ở thủ đô Moscow để dự lễ duyệt binh kỷ niệm 79 năm Ngày Chiến thắng trong Chiến tranh Vệ quốc vĩ đại.",
+        "likes": 34,
+        "comments": 8
+    },
+    {
+        "id": 4,
+        "title": "Đã có hàng nghìn lượt xem, góp ý sửa Hiến pháp trên cổng thông tin điện tử",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1EirAD.img?w=268&h=140&q=60&m=6&f=jpg&u=t",
+        "source": "Lao Động",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1IQAXX?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "4 giờ",
+        "content": "Sau gần 1 tuần vận hành, Cổng thông tin điện tử góp ý dự thảo sửa đổi Hiến pháp đã thu hút được hàng nghìn lượt truy cập và hàng trăm ý kiến đóng góp.",
+        "likes": 18,
+        "comments": 6
+    },
+    {
+        "id": 5,
+        "title": "Thường trực Ban Bí thư sẽ làm việc với các tỉnh thành về Quy hoạch",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1Ei8J5.img?w=268&h=140&q=60&m=6&f=jpg&u=t",
+        "source": "VnExpress",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB131kvb?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "3 giờ",
+        "content": "Thường trực Ban Bí thư Lương Cường sẽ làm việc với lãnh đạo các tỉnh, thành phố về việc triển khai thực hiện Quy hoạch tổng thể quốc gia thời kỳ 2021 - 2030.",
+        "likes": 15,
+        "comments": 2
+    },
+    {
+        "id": 6,
+        "title": "Điều gì xảy ra khi ăn bánh ngọt buổi sáng?",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1Ei10s.img?w=485&h=273&q=60&m=6&f=jpg&u=t",
+        "source": "VnExpress",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB131kvb?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "3 ngày",
+        "content": "Bánh ngọt chứa nhiều đường và chất béo có thể gây tăng lượng đường trong máu đột ngột, dẫn đến cảm giác mệt mỏi và đói nhanh chóng vào buổi sáng.",
+        "likes": 11,
+        "comments": 4
+    },
+    {
+        "id": 7,
+        "title": "4 loại thiết bị nên rút điện khi không ở nhà",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1EguWO.img?w=485&h=273&q=60&m=6&f=jpg&u=t",
+        "source": "VnExpress",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB131kvb?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "22 giờ",
+        "content": "Các chuyên gia khuyên không nên để những thiết bị như bàn là, máy sạc, máy giặt, máy rửa bát cắm điện khi không có người ở nhà để tránh nguy cơ cháy nổ.",
+        "likes": 33,
+        "comments": 9
+    },
+    {
+        "id": 8,
+        "title": "Thu giữ hơn 3 tạ rau quả nhiễm hóa chất độc hại",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1Eir75.img?w=485&h=273&q=60&m=6&f=jpg&u=t",
+        "source": "Tuổi trẻ",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB9mld4?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "2 giờ",
+        "content": "Đoàn kiểm tra liên ngành đã phát hiện và thu giữ hơn 300kg rau quả các loại nhiễm hóa chất độc hại tại một chợ đầu mối ở TP.HCM.",
+        "likes": 5,
+        "comments": 1
+    },
+    {
+        "id": 9,
+        "title": "5 loại nước gây hại cho thận",
+        "image": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1Eibb9.img?w=485&h=273&q=60&m=6&f=jpg&u=t",
+        "source": "Ngôi sao",
+        "source_logo": "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1vO4hY?w=16&h=16&q=100&m=6&f=png&u=t",
+        "time": "2 tuần",
+        "content": "Các chuyên gia cho biết, nước ngọt có ga, rượu bia, nước tăng lực, nước trái cây có đường và cà phê có thể gây hại cho thận nếu sử dụng thường xuyên.",
+        "likes": 195,
+        "comments": 37
+    }
+]
+
+FINANCIAL_DATA = [
+    {
+        "name": "JPY/VND",
+        "description": "Đồng Yên Nhật/Việt Nam",
+        "value": "181.17",
+        "change": "-0.54%",
+        "up": False
+    },
+    {
+        "name": "Citigroup Inc",
+        "description": "C",
+        "value": "69.47",
+        "change": "-1.10%",
+        "up": False
+    },
+    {
+        "name": "KRW/VND",
+        "description": "Đồng Won của Hàn Quốc/Việt Nam",
+        "value": "18.629",
+        "change": "-1.16%",
+        "up": False
+    },
+    {
+        "name": "USD/JPY",
+        "description": "Đô la Mỹ/Đồng Yên Nhật",
+        "value": "143.24",
+        "change": "+0.58%",
+        "up": True
+    },
+    {
+        "name": "JPY/USD",
+        "description": "Đồng Yên Nhật/Đô la Mỹ",
+        "value": "0.006981",
+        "change": "-0.56%",
+        "up": False
+    }
+]
+
+CATEGORIES = [
+    {"id": "khampla", "name": "Khám phá"},
+    {"id": "tintuc", "name": "Tin tức"},
+    {"id": "thethao", "name": "Thể thao"},
+    {"id": "taichinh", "name": "Tài chính"},
+    {"id": "thoitiet", "name": "Thời tiết"},
+    {"id": "giaitri", "name": "Giải trí"},
+    {"id": "cunghoangdao", "name": "Cung hoàng đạo"},
+    {"id": "tindiadiem", "name": "Tin địa điểm"},
+    {"id": "phapduy", "name": "Pháp luật"}
+]
+
+QUICK_LINKS = [
+    {
+        "name": "Outlook.com",
+        "icon": "https://ext.same-assets.com/2035564999/1485819331.svg",
+        "url": "https://outlook.com"
+    },
+    {
+        "name": "Lazada",
+        "icon": "https://logowik.com/content/uploads/images/lazada2906.jpg",
+        "url": "https://lazada.vn",
+        "is_trusted": True
+    },
+    {
+        "name": "Shopee",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Shopee.svg/1200px-Shopee.svg.png",
+        "url": "https://shopee.vn",
+        "is_trusted": True
+    },
+    {
+        "name": "Facebook",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png",
+        "url": "https://facebook.com"
+    },
+    {
+        "name": "Microsoft Store",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Microsoft_Store_icon.svg/1200px-Microsoft_Store_icon.svg.png",
+        "url": "https://www.microsoft.com/store/apps/windows"
+    },
+    {
+        "name": "Microsoft 365",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Microsoft_Office_logo_%282019%E2%80%93present%29.svg/1200px-Microsoft_Office_logo_%282019%E2%80%93present%29.svg.png",
+        "url": "https://www.microsoft.com/microsoft-365"
+    },
+    {
+        "name": "Skype",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Skype_logo_%282019%E2%80%93present%29.svg/1200px-Skype_logo_%282019%E2%80%93present%29.svg.png",
+        "url": "https://skype.com"
+    },
+    {
+        "name": "OneNote",
+        "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Microsoft_Office_OneNote_%282019%E2%80%93present%29.svg/1200px-Microsoft_Office_OneNote_%282019%E2%80%93present%29.svg.png",
+        "url": "https://www.onenote.com"
+    }
+]

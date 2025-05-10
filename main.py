33b416331dadcfb79167ffbb829b45d0c35b3655
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-05-10 17:47:31 +0700

    Update somethings

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from scraper import scrape_cnn_articles
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the CNN News Scraper API!"}
+
 @app.get("/scrape")
-def get_articles():
+async def get_articles():
     articles = scrape_cnn_articles()
     return {"articles": articles}

afaa55e868a25fd726b8aa1273456fdc39bbe01f
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-05-10 18:16:30 +0700

    Update somethings

--- local/app.py
+++ local/app.py
@@ -7,13 +7,16 @@ def display_articles(articles):
         st.write(f"[Read more]({article['url']})")
         st.write("---")
 
+# http://127.0.0.1:8000
+
 def main():
     st.title("CNN News Scraper")
     st.write("This app scrapes the latest articles from CNN using an API.")
 
     # Gọi API từ FastAPI
-    api_url = "http://127.0.0.1:8000/scrape"# Đổi URL nếu cần
+    api_url = "https://nlpapplication.onrender.com/scrape"# Đổi URL nếu cần
     response = requests.get(api_url)
+    print(response.json())
     
     if response.status_code == 200:
         data = response.json()
--- scraper.py
+++ scraper.py
@@ -1,21 +1,23 @@
-# scraper.py
 import httpx
 from bs4 import BeautifulSoup
 
 async def scrape_cnn_articles():
-    url = "https://edition.cnn.com/world"
+    url = "https://edition.cnn.com/us"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
 
         articles = []
-        for link in soup.select("h3 a"):
-            href = link.get("href")
-            title = link.text.strip()
+        for a_tag in soup.select("a.container__link--type-article"):
+            href = a_tag.get("href")
+            headline_span = a_tag.select_one("span.container__headline-text")
+            title = headline_span.get_text(strip=True) if headline_span else None
+
             if href and title:
+                full_url = f"https://edition.cnn.com{href}" if href.startswith("/") else href
                 articles.append({
                     "title": title,
-                    "url": f"https://edition.cnn.com{href}" if href.startswith("/") else href
+                    "url": full_url
                 })
 
         return articles
